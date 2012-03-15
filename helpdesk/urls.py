9ace0d3fa3fa93f7628534023b177d37fe0e1fa8
Michael Anderson <andermic@steel.andermic.com>
2012-03-14 23:02:44 -0500

    switched to pinax

--- helpdesk/urls.py
+++ helpdesk/urls.py
@@ -5,10 +5,28 @@ from django.views.generic.simple import direct_to_template
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^$', direct_to_template, { "template": "homepage.html",}, name="home"),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^about/', include("helpdesk.about.urls")),
+from pinax.apps.account.openid_consumer import PinaxConsumer
+
+
+handler500 = "pinax.views.server_error"
+
+
+urlpatterns = patterns("",
+    url(r"^$", direct_to_template, {
+        "template": "homepage.html",
+    }, name="home"),
+    url(r"^admin/invite_user/$", "pinax.apps.signup_codes.views.admin_invite_user", name="admin_invite_user"),
+    url(r"^admin/", include(admin.site.urls)),
+    url(r"^about/", include("about.urls")),
+    url(r"^account/", include("pinax.apps.account.urls")),
+    url(r"^openid/", include(PinaxConsumer().urls)),
+    url(r"^profiles/", include("idios.urls")),
+    url(r"^notices/", include("notification.urls")),
+    url(r"^announcements/", include("announcements.urls")),
 )
+
+
+if settings.SERVE_MEDIA:
+    urlpatterns += patterns("",
+        url(r"", include("staticfiles.urls")),
+    )
